32223fd7764bc3145f83b1510ba8d95d5cba57f2
choutaLAN <117169061+choutaLAN@users.noreply.github.com>
2024-07-21 11:00:52 +1000

    Initial

---
+++ Mainwindow.cpp
@@ -0,0 +1,14 @@
+#include "Mainwindow.hpp"
+#include "ui_Mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
