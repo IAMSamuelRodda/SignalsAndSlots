831cb7bc9b6163f6c63e3986d4acad72587c992b
choutaLAN <117169061+choutaLAN@users.noreply.github.com>
2024-07-21 20:30:57 +1000

    Added addition calculator function

--- Mainwindow.cpp
+++ Mainwindow.cpp
@@ -12,3 +12,14 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+int FirstNum, SecondNum, Result;
+
+void MainWindow::on_PlusPushButton_clicked()
+{
+    FirstNum = ui -> Txt1stNumber -> text().toInt();
+    SecondNum = ui -> Txt2ndNumber -> text().toInt();
+    Result = FirstNum + SecondNum;
+    ui -> TxtResult -> setText(QString::number(Result));
+}
+
--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "Mainwindow.hpp"
 
 #include <QApplication>
-
+//#include <iostream>
+//using namespace std;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    //cout << "test";
     return a.exec();
 }
